f09050773f7fc5138596a383113db20c4f558f36
MATRIX-DATA\ashah <abu.shah@strategic-i.com>
2022-01-31 12:05:46 +0000

    Added an enum BookGenres

---
+++ LibraryManager.Core/BookGenre.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Core
+{
+    public enum BookGenre
+    {
+        None,
+        Science,
+        Religion,
+        History,
+        Other
+    }
+}
