863bd79d8aa75e583b38d54ee17e4b4cbcc01910
MATRIX-DATA\ashah <abu.shah@strategic-i.com>
2022-01-31 22:02:36 +0000

    Added new project LIbraryManager.Data | Added a an in memory data repository

---
+++ LibraryManager.Data/IBookData.cs
@@ -0,0 +1,13 @@
+﻿using LibraryManager.Core;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManager.Data
+{
+    public interface IBookData
+    {
+        IEnumerable<Book> GetAllBooks();
+    }
+}
---
+++ LibraryManager.Data/InMemoryBookData.cs
@@ -0,0 +1,28 @@
+﻿using LibraryManager.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LibraryManager.Data
+{
+    public class InMemoryBookData : IBookData
+    {
+        List<Book> books;
+
+        public InMemoryBookData()
+        {
+            books = new List<Book>()
+            {
+                new Book { Id = 1, Title = "Learn C# in One Day and Learn It Well", BookGenre = BookGenre.Science},
+                new Book { Id = 2, Title = "12 Major World Religions", BookGenre = BookGenre.Religion},
+                new Book { Id = 3, Title = "Humankind: A Hopeful History", BookGenre = BookGenre.History},
+                new Book { Id =4, Title = "Soft Skills: The Software Developer's Life Manual", BookGenre = BookGenre.Other}
+            };
+        }
+        public IEnumerable<Book> GetAllBooks()
+        {
+            return books.OrderBy(b => b.Title).ToList();
+
+        }
+    }
+}
