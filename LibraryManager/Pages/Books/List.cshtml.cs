99cabbfd6a0b0a6084f06cf0ac1fa45f4e3c7fce
MATRIX-DATA\ashah <abu.shah@strategic-i.com>
2022-01-31 22:03:12 +0000

    The List page shows a list of books

--- LibraryManager/Pages/Books/List.cshtml.cs
+++ LibraryManager/Pages/Books/List.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LibraryManager.Core;
+using LibraryManager.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,16 @@ namespace LibraryManager.Pages.Books
 {
     public class ListModel : PageModel
     {
+        private readonly IBookData books;
+        public IEnumerable<Book> Books { get; set; }
+
+        public ListModel(IBookData books)
+        {
+            this.books = books;
+        }
         public void OnGet()
         {
+            Books = books.GetAllBooks();
         }
     }
 }
