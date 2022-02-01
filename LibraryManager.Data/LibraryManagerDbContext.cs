242fc74bcd1563eb7abb733101cd4cc52fc4e906
MATRIX-DATA\ashah <abu.shah@strategic-i.com>
2022-02-01 23:49:02 +0000

    Added database | EF packages | DBContext | Intial migration

---
+++ LibraryManager.Data/LibraryManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LibraryManager.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryManager.Data
+{
+    public class LibraryManagerDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=C:\Dev\LearningAndReference\shared projects\LibraryManagerDB\LibraryManagerDB.db;");
+        }
+    }
+}
---
+++ LibraryManager.Data/Migrations/20220201234126_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryManager.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(maxLength: 300, nullable: false),
+                    ISBN = table.Column<string>(maxLength: 20, nullable: true),
+                    BookGenre = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
