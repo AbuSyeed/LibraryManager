643e5a05605332ebc144bfa3b6210863bba0a35c
MATRIX-DATA\ashah <abu.shah@strategic-i.com>
2022-02-01 22:08:00 +0000

    Added validation rules

--- LibraryManager.Core/Book.cs
+++ LibraryManager.Core/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace LibraryManager.Core
@@ -7,7 +8,9 @@ namespace LibraryManager.Core
     public class Book
     {
         public int Id { get; set; }
+        [Required, StringLength(300)]
         public string Title { get; set; }
+        [StringLength(20)]
         public string ISBN { get; set; }
         public BookGenre BookGenre { get; set; }
     }
